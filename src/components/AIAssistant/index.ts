f6026372c2b0199ce46ebfb26647dc0fbf755248
shalharisi <nursing.aljanbah.phcc@gmail.com>
2026-05-11 20:20:03 +0300

    Export AI Assistant components

---
+++ src/components/AIAssistant/index.ts
@@ -0,0 +1,3 @@
+export { AIAssistantProvider, useAIAssistant } from './AIAssistantContext';
+export type { AIAssistantContextType, Message } from './AIAssistantContext';
+export { AIAssistantChat } from './AIAssistantChat';
